4266d7a410d771a555af6f42277d7848c762d491
timur.mukhortov <t.mukhortov@corp.mail.ru>
2019-10-02 00:51:56 +0300

    Made thread safe instance network service

--- app/src/main/java/me/tmukhortov/zimad/utility/network/NetworkService.java
+++ app/src/main/java/me/tmukhortov/zimad/utility/network/NetworkService.java
@@ -6,7 +6,7 @@ import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
 public class NetworkService {
-    private static NetworkService mInstance;
+    private volatile static NetworkService mInstance;
     private static final String BASE_URL = "http://kot3.com/xim/";
     private Retrofit mRetrofit;
 
