553ec43d7a8e79cbfadc43ba6232221a78f87ba3
t.mukhortov <t.mukhortov@gmail.com>
2019-10-20 23:29:01 +0300

    Added AnimalDescriptionFragment

---
+++ app/src/main/java/me/tmukhortov/zimad/presentation/animal/item/fragment/AnimalDescriptionFragment.java
@@ -0,0 +1,23 @@
+package me.tmukhortov.zimad.presentation.animal.item.fragment;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import me.tmukhortov.zimad.R;
+
+public class AnimalDescriptionFragment extends Fragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
+                             @Nullable Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.fragment_animal_description, container, false);
+        // TODO get data from dog/cat screen and set to view
+        return rootView;
+    }
+}
