1460aa3e82869ecb322d01525ad8f34b6cd1ae1e
t.mukhortov <t.mukhortov@gmail.com>
2019-10-22 16:09:40 +0300

    Add interface for navigation between fragments

--- app/src/main/java/me/tmukhortov/zimad/utility/navigation/Navigation.java
+++ app/src/main/java/me/tmukhortov/zimad/utility/navigation/Navigation.java
@@ -4,6 +4,20 @@ import androidx.fragment.app.Fragment;
 
 public interface Navigation {
 
+    /**
+     *
+     * Open specific fragment
+     * @param fragment
+     */
     void navigateTo(Fragment fragment);
 
+    /**
+     * Back to previous screen in stack
+     */
+    void back();
+
+    /**
+     * Back to specific fragment
+     */
+    void backTo();
 }
