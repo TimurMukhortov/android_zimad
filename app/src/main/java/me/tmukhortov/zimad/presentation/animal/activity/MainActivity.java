9c9275838e4b4cbf9fd279f07089f775a9800d34
t.mukhortov <t.mukhortov@gmail.com>
2019-11-23 18:05:46 +0300

    Replace all logic with tabs in TabFragment

--- app/src/main/java/me/tmukhortov/zimad/presentation/animal/activity/MainActivity.java
+++ app/src/main/java/me/tmukhortov/zimad/presentation/animal/activity/MainActivity.java
@@ -8,70 +8,72 @@ import me.tmukhortov.zimad.R;
 import me.tmukhortov.zimad.ZimadApplication;
 import me.tmukhortov.zimad.presentation.animal.animal_list.fragment.CatListFragment;
 import me.tmukhortov.zimad.presentation.animal.animal_list.fragment.DogListFragment;
+import me.tmukhortov.zimad.presentation.animal.animal_list.fragment.TabFragment;
 import me.tmukhortov.zimad.presentation.base.activity.BaseActivity;
 
 public class MainActivity extends BaseActivity {
 
-    private static final String TAB_STATE = "tab_state";
-
-    private int selectedTab = -1;
-    private TabLayout.BaseOnTabSelectedListener bottomBarTabSelectedListener;
-    private TabLayout bottomBar;
+//    private static final String TAB_STATE = "tab_state";
+//
+//    private int selectedTab = -1;
+//    private TabLayout.BaseOnTabSelectedListener bottomBarTabSelectedListener;
+//    private TabLayout bottomBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        bottomBar = findViewById(R.id.activity_main_tab_layout_action);
-        bottomBarTabSelectedListener = new TabLayout.BaseOnTabSelectedListener() {
-            @Override
-            public void onTabSelected(TabLayout.Tab tab) {
-                switch (tab.getPosition()) {
-                    case 0: {
-                        selectedTab = 0;
-                        ZimadApplication.INSTANCE.getNavigationHolder().getNavigator()
-                                                 .replace(CatListFragment.newInstance());
-                        break;
-                    }
-                    case 1: {
-                        selectedTab = 1;
-                        ZimadApplication.INSTANCE.getNavigationHolder().getNavigator()
-                                                 .replace(DogListFragment.newInstance());
-                    }
-                }
-            }
-
-            @Override
-            public void onTabUnselected(TabLayout.Tab tab) {
-            }
-
-            @Override
-            public void onTabReselected(TabLayout.Tab tab) {
-            }
-        };
-        bottomBar.addOnTabSelectedListener(bottomBarTabSelectedListener);
-        if (savedInstanceState == null) {
-            selectedTab = 0;
-            ZimadApplication.INSTANCE.getNavigationHolder().getNavigator()
-                                     .navigateTo(CatListFragment.newInstance());
-        } else {
-            int positionTab = savedInstanceState.getInt(TAB_STATE);
-            highlightBottomBarMenu(positionTab);
-        }
+        ZimadApplication.INSTANCE.getNavigationHolder().getNavigator().replace(TabFragment.newInstance());
+//        bottomBar = findViewById(R.id.activity_main_tab_layout_action);
+//        bottomBarTabSelectedListener = new TabLayout.BaseOnTabSelectedListener() {
+//            @Override
+//            public void onTabSelected(TabLayout.Tab tab) {
+//                switch (tab.getPosition()) {
+//                    case 0: {
+//                        selectedTab = 0;
+//                        ZimadApplication.INSTANCE.getNavigationHolder().getNavigator()
+//                                                 .replace(CatListFragment.newInstance());
+//                        break;
+//                    }
+//                    case 1: {
+//                        selectedTab = 1;
+//                        ZimadApplication.INSTANCE.getNavigationHolder().getNavigator()
+//                                                 .replace(DogListFragment.newInstance());
+//                    }
+//                }
+//            }
+//
+//            @Override
+//            public void onTabUnselected(TabLayout.Tab tab) {
+//            }
+//
+//            @Override
+//            public void onTabReselected(TabLayout.Tab tab) {
+//            }
+//        };
+//        bottomBar.addOnTabSelectedListener(bottomBarTabSelectedListener);
+//        if (savedInstanceState == null) {
+//            selectedTab = 0;
+//            ZimadApplication.INSTANCE.getNavigationHolder().getNavigator()
+//                                     .navigateTo(CatListFragment.newInstance());
+//        } else {
+//            int positionTab = savedInstanceState.getInt(TAB_STATE);
+//            highlightBottomBarMenu(positionTab);
+//        }
     }
 
-    @Override
-    protected void onSaveInstanceState(@NonNull Bundle outState) {
-        super.onSaveInstanceState(outState);
-        outState.putInt(TAB_STATE, selectedTab);
-    }
+//    @Override
+//    protected void onSaveInstanceState(@NonNull Bundle outState) {
+//        super.onSaveInstanceState(outState);
+//        outState.putInt(TAB_STATE, selectedTab);
+//    }
 
-    private void highlightBottomBarMenu(int position) {
-        bottomBar.removeOnTabSelectedListener(bottomBarTabSelectedListener);
-        TabLayout.Tab tab = bottomBar.getTabAt(position);
-        if (tab != null) {
-            tab.select();
-        }
-        bottomBar.addOnTabSelectedListener(bottomBarTabSelectedListener);
-    }
+//    private void highlightBottomBarMenu(int position) {
+//        bottomBar.removeOnTabSelectedListener(bottomBarTabSelectedListener);
+//        TabLayout.Tab tab = bottomBar.getTabAt(position);
+//        if (tab != null) {
+//            tab.select();
+//        }
+//        bottomBar.addOnTabSelectedListener(bottomBarTabSelectedListener);
+//    }
 }
--- app/src/main/java/me/tmukhortov/zimad/presentation/animal/animal_list/fragment/TabFragment.java
+++ app/src/main/java/me/tmukhortov/zimad/presentation/animal/animal_list/fragment/TabFragment.java
@@ -8,9 +8,22 @@ import android.view.ViewGroup;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import com.google.android.material.tabs.TabLayout;
 import me.tmukhortov.zimad.R;
+import me.tmukhortov.zimad.ZimadApplication;
 
 public class TabFragment extends Fragment {
+
+    private static final String TAB_STATE = "tab_state";
+
+    private int selectedTab = -1;
+    private TabLayout.BaseOnTabSelectedListener bottomBarTabSelectedListener;
+    private TabLayout bottomBar;
+
+    public static TabFragment newInstance(){
+        TabFragment tabFragment = new TabFragment();
+        return tabFragment;
+    }
 
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
@@ -22,6 +35,57 @@ public class TabFragment extends Fragment {
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
                              @Nullable Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.fragment_animals_tabs, container, false);
+        bottomBar = rootView.findViewById(R.id.activity_main_tab_layout_action);
+        bottomBarTabSelectedListener = new TabLayout.BaseOnTabSelectedListener() {
+            @Override
+            public void onTabSelected(TabLayout.Tab tab) {
+                switch (tab.getPosition()) {
+                    case 0: {
+                        selectedTab = 0;
+                        ZimadApplication.INSTANCE.getNavigationHolder().getNavigator()
+                                                 .replace(CatListFragment.newInstance());
+                        break;
+                    }
+                    case 1: {
+                        selectedTab = 1;
+                        ZimadApplication.INSTANCE.getNavigationHolder().getNavigator()
+                                                 .replace(DogListFragment.newInstance());
+                    }
+                }
+            }
+
+            @Override
+            public void onTabUnselected(TabLayout.Tab tab) {
+            }
+
+            @Override
+            public void onTabReselected(TabLayout.Tab tab) {
+            }
+        };
+        bottomBar.addOnTabSelectedListener(bottomBarTabSelectedListener);
+        if (savedInstanceState == null) {
+            selectedTab = 0;
+            ZimadApplication.INSTANCE.getNavigationHolder().getNavigator()
+                                     .navigateTo(CatListFragment.newInstance());
+        } else {
+            int positionTab = savedInstanceState.getInt(TAB_STATE);
+            highlightBottomBarMenu(positionTab);
+        }
         return rootView;
+    }
+
+    @Override
+    public void onSaveInstanceState(@NonNull Bundle outState) {
+        super.onSaveInstanceState(outState);
+        outState.putInt(TAB_STATE, selectedTab);
+    }
+
+    private void highlightBottomBarMenu(int position) {
+        bottomBar.removeOnTabSelectedListener(bottomBarTabSelectedListener);
+        TabLayout.Tab tab = bottomBar.getTabAt(position);
+        if (tab != null) {
+            tab.select();
+        }
+        bottomBar.addOnTabSelectedListener(bottomBarTabSelectedListener);
     }
 }
